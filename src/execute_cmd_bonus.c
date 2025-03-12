e71faf28e79daf67dabd32707a58fde3e641bc79
Meera Shersheer <meera04qasemshersheer@gmail.com>
2025-03-12 08:52:39 +0300

    bulding the multiple pipe bonus

--- src/execute_cmd_bonus.c
+++ src/execute_cmd_bonus.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   execute_cmd_bonus.c                                :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: mshershe <mshershe@student.42.fr>          +#+  +:+       +#+        */
+/*   By: mshershe <mshershe@student.42amman.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/09 15:00:34 by mshershe          #+#    #+#             */
-/*   Updated: 2025/03/11 16:39:56 by mshershe         ###   ########.fr       */
+/*   Updated: 2025/03/12 08:52:14 by mshershe         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -73,28 +73,6 @@ void    exceute_cmd_out(char **cmd,t_dlist **list, char *outfile,int *pipe_fd)
 	close(fd);
 }
 
-void pipex(char **args1, char **args2, char *infile, char *outfile)
-{	
-	pid_t id;
-	int status;
-	int pipefd[2];
-	
-	if (pipe(pipefd) == -1)
-		exit_program(args1, args2, -1, -1);
-	id = fork();
-	if(id == -1)
-		exit_program(args1, args2, -1, -1);
-    else if(id == 0)
-    {
-		exceute_cmd_in(args1,args2, infile, pipefd);
-	}   
-    else
-	{
-		waitpid(id, &status, 0);
-		exceute_cmd_out(args2, args1, outfile, pipefd);
-	}
-	
-}
 void pipex_multi(t_dlist *list, char *infile, char *outfile)
 {	
 	pid_t id;
@@ -114,7 +92,7 @@ void pipex_multi(t_dlist *list, char *infile, char *outfile)
 		list = list->next;
 		while(n_pipes < 0)
 		{
-			
+			exceute_cmd(list->cmd ,&list, pipefd);// new function the read from the pipe and write to it 
 			n_pipes--;
 			list = list->next;
 		}
@@ -122,7 +100,37 @@ void pipex_multi(t_dlist *list, char *infile, char *outfile)
     else
 	{
 		waitpid(id, &status, 0);
-		exceute_cmd_out(args2, args1, outfile, pipefd);
+		exceute_cmd_out(list->cmd ,&list, outfile, pipefd);
 	}
 	
+}
+//pipe_fd[0]-> read side of the pipe ->> write data to the output
+//pipe_fd[1]-> write side of the pipe ->> read data from input
+void    exceute_cmd(char **cmd,t_dlist **list,int *pipe_fd) /// need modifying (didn't work on the body yet)
+{
+    int fd;
+    pid_t id;
+	int status;
+
+	close(pipe_fd[0]);
+    fd = open(infile, O_RDONLY);
+    if (fd == -1)
+		exit_pipex(list, -1, pipe_fd[1]);
+	if (dup2(fd, STDIN_FILENO) == -1)
+		exit_pipex(list, fd, pipe_fd[1]);
+	if(dup2(pipe_fd[1], STDOUT_FILENO) == -1)
+		exit_pipex(list, fd, pipe_fd[1]);
+    id = fork();
+	if(id == -1)
+		exit_pipex(list, fd, pipe_fd[1]);
+    else if(id == 0)
+        execve(cmd[0], cmd, NULL);
+    else
+	{
+		waitpid(id, &status, 0);
+		if (WEXITSTATUS(status) != 0)
+			exit_pipex(list, fd, pipe_fd[1]);
+	}
+	close(pipe_fd[1]);
+	close(fd);
 }
